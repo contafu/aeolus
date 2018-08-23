3f82a29ba692a1428c58eab6647253e85f6253b1
tomas <>
2018-08-23 10:36:46 +0800

    bug fix

--- sample/src/main/java/com/olympians/aeolus/Request.java
+++ sample/src/main/java/com/olympians/aeolus/Request.java
@@ -2,7 +2,6 @@ package com.olympians.aeolus;
 
 import com.olympians.aeolus.annotations.Get;
 
-@Get(host = "http://localhost", api = "mobile/common-filter-menu-info-list"
-)
+@Get(host = "http://192.168.1.20:80", api = "mobile/search")
 public class Request implements AeolusRequest {
 }
