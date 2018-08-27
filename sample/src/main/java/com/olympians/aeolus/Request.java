75f2f752d54fbb5af893fdc3f8837cdc1af8c846
tomas <>
2018-08-27 14:05:20 +0800

    添加过滤404错误

--- sample/src/main/java/com/olympians/aeolus/Request.java
+++ sample/src/main/java/com/olympians/aeolus/Request.java
@@ -2,6 +2,6 @@ package com.olympians.aeolus;
 
 import com.olympians.aeolus.annotations.Get;
 
-@Get(host = "http://192.168.1.20:80", api = "mobile/search")
+@Get(host = "http://192.168.1.20", api = "/mobile/app-name")
 public class Request implements AeolusRequest {
 }
