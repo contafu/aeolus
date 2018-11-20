94d0a5387463793c5583f5376cacf759b214beef
tomas <>
2018-11-20 19:00:11 +0800

    common submit

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ let app = new express();
 app.use('/mobile', require('./router/index'));
 
 app.listen(3000, () => {
-    console.log('starting... welcome to visit "http://localhost:3000/mobile/"')
+    console.log("starting... welcome to visit 'http://localhost:3000/mobile/'")
 })
--- server/router/index.js
+++ server/router/index.js
@@ -2,26 +2,26 @@ let router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.json({
-        province: '台湾省',
+        province: '臺灣省',
         citys: [
             {
-                city: '台北'
+                city: '台北市'
             }, {
-                city: '新北'
+                city: '新北市'
             }, {
-                city: '桃园'
+                city: '桃園市'
             }, {
-                city: '台中'
+                city: '臺中市'
             }, {
-                city: '台南'
+                city: '臺南市'
             }, {
-                city: '高雄'
+                city: '高雄市'
             }, {
-                city: '基隆'
+                city: '基隆市'
             }, {
-                city: '新竹'
+                city: '新竹市'
             }, {
-                city: '嘉义'
+                city: '嘉義市'
             }
         ]
     });
