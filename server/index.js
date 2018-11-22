cd5a2b87f488707d806ea5b795117519cabf6208
tomas <>
2018-11-22 11:38:14 +0800

    添加上传文件服务器

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-let express = require('express');
-
-let app = new express();
+const express = require('express');
+const app = new express();
 
 app.use('/mobile', require('./router/index'));
 
+app.use((err, req, res, next) => {
+    res.send(err);
+});
+
 app.listen(3000, () => {
     console.log("starting... welcome to visit 'http://localhost:3000/mobile/'")
-})
+});
--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'}).any();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     res.json({
         province: '臺灣省',
-        citys: [
+        cities: [
             {
                 city: '台北市'
             }, {
@@ -26,3 +29,32 @@ router.get('/', (req, res) => {
         ]
     });
 });
+
+router.post('/upload', (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err) {
+            next(err);
+        }
+        req.file = req.files[0];
+        let tmp_path = req.file.path;
+        let target_path = 'uploads/' + req.file.originalname;
+        if (!fs.existsSync('uploads/')) {
+            fs.mkdirSync('uploads/')
+        }
+
+        let src = fs.createReadStream(tmp_path);
+        let dest = fs.createWriteStream(target_path);
+        src.pipe(dest);
+        src.on('end', (data) => {
+            res.json({
+                code: 0,
+                msg: '上传成功'
+            })
+        });
+        src.on('error', (err) => {
+            next(err);
+        });
+    });
+});
+
+module.exports = router;
